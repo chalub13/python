af0bf615aad97d72efd9671798cc7e2033390dcd
Marco Chalub <marco.chalub@gmail.com>
2019-10-01 14:52:17 +1000

    Basics

--- Learning Python/Ch2/classes_exercise.py
+++ Learning Python/Ch2/classes_exercise.py
@@ -3,8 +3,30 @@
 #
 
 
+class myClass:
+    def method1(self):
+        print("myClass method1")
+
+    def method2(self, someString):
+        print("myClass method2 " + someString)
+
+
+class anotherClass(myClass):
+    def method1(self):
+        myClass.method1(self)
+        print("anotherClass method1")
+
+    def method2(self, someString):
+        print("anotherClass method2 " + someString)
+
+
 def main():
-    print("Hello World")
+    c = myClass()
+    c.method1()
+    c.method2("This is a string")
+    c2 = anotherClass()
+    c2.method1()
+    c2.method2("This is another string")
 
 
 if __name__ == "__main__":
--- Learning Python/Ch2/loops_exercise.py
+++ Learning Python/Ch2/loops_exercise.py
@@ -2,23 +2,42 @@
 # Example file for working with loops
 #
 
+
 def main():
-  x = 0
+    x = 0
 
-  # define a while loop
+    # define a while loop
+    print("define a while loop")
+    while x < 5:
+        print(x)
+        x += 1
 
+    # define a for loop
+    print("define a for loop")
+    for i in range(5, 10, 2):
+        print(i)
 
-  # define a for loop
+    # use a for loop over a collection
+    print("use a for loop over a collection")
+    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    for x in days:
+        print(x)
 
+    # use the break and continue statements
+    print("use the break and continue statements")
+    for i in range(5, 10):
+        # if i == 7:
+        #     break
+        if i % 2 == 0:
+            continue
+        print(i)
 
-  # use a for loop over a collection
-    
- 
-  # use the break and continue statements
-
-
-  #using the enumerate() function to get index 
+    # using the enumerate() function to get index
+    print("using the enumerate() function to get index")
+    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    for i, d in enumerate(days):
+        print(i, d)
 
 
 if __name__ == "__main__":
-  main()
+    main()
